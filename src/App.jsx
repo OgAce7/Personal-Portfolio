77f59621b6f54f708670f895aa40edd645711665
OgAce7 <devanshtiwari748@gmail.com>
2026-07-31 23:24:34 +0530

    added missing sections

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Hero from './sections/Hero';
 import About from './sections/About';
 import Skills from './sections/Skills';
 import Projects from './sections/Projects';
+import Experience from './sections/Experience';
+import Education from './sections/Education';
+import Achievements from './sections/Achievements';
 import Contact from './sections/Contact';
 import Footer from './sections/Footer';
 
